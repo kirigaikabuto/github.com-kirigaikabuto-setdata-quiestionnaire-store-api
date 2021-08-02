74a2abea0d3476fea2fa9e2f694b8c9fad910f38
Yerassyl <tleugazy98@gmail.com>
2021-08-02 19:00:58 +0600

    commits

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.13
 
 require (
 	github.com/djumanoff/amqp v1.0.6
-	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.3.0
 	github.com/kirigaikabuto/setdata-common v1.0.1-0.20210802114433-ce62b657191a
-	github.com/kirigaikabuto/setdata-questionnaire-store v0.0.1 // indirect
+	github.com/kirigaikabuto/setdata-questionnaire-store v0.0.2-0.20210802125916-9f06828b2d66 // indirect
+	github.com/urfave/cli v1.22.5
 )
--- main.go
+++ main.go
@@ -1,5 +1,136 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/djumanoff/amqp"
+	"github.com/joho/godotenv"
+	setdata_common "github.com/kirigaikabuto/setdata-common"
+	setdata_questionnaire_store "github.com/kirigaikabuto/setdata-questionnaire-store"
+	"github.com/urfave/cli"
+	"os"
+	"strconv"
+)
 
+var (
+	configPath           = ".env"
+	version              = "0.0.0"
+	amqpHost             = ""
+	amqpPort             = 0
+	postgresUser         = ""
+	postgresPassword     = ""
+	postgresDatabaseName = ""
+	postgresHost         = ""
+	postgresPort         = 5432
+	postgresParams       = ""
+	flags                = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "config, c",
+			Usage:       "path to .env config file",
+			Destination: &configPath,
+		},
+	}
+)
+
+func parseEnvFile() {
+	// Parse config file (.env) if path to it specified and populate env vars
+	if configPath != "" {
+		godotenv.Overload(configPath)
+	}
+	amqpHost = os.Getenv("RABBIT_HOST")
+	amqpPortStr := os.Getenv("RABBIT_PORT")
+	amqpPort, _ = strconv.Atoi(amqpPortStr)
+	if amqpPort == 0 {
+		amqpPort = 5672
+	}
+	if amqpHost == "" {
+		amqpHost = "localhost"
+	}
+	postgresUser = os.Getenv("POSTGRES_USER")
+	postgresPassword = os.Getenv("POSTGRES_PASSWORD")
+	postgresDatabaseName = os.Getenv("POSTGRES_DATABASE")
+	postgresParams = os.Getenv("POSTGRES_PARAMS")
+	portStr := os.Getenv("POSTGRES_PORT")
+	postgresPort, _ = strconv.Atoi(portStr)
+	postgresHost = os.Getenv("POSTGRES_HOST")
+	if postgresHost == "" {
+		postgresHost = "localhost"
+	}
+	if postgresPort == 0 {
+		postgresPort = 5432
+	}
+	if postgresUser == "" {
+		postgresUser = "setdatauser"
+	}
+	if postgresPassword == "" {
+		postgresPassword = "123456789"
+	}
+	if postgresDatabaseName == "" {
+		postgresDatabaseName = "setdata"
+	}
+	if postgresParams == "" {
+		postgresParams = "sslmode=disable"
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Set data questions api"
+	app.Description = ""
+	app.Usage = "set data run"
+	app.UsageText = "set data run"
+	app.Version = version
+	app.Flags = flags
+	app.Action = run
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	parseEnvFile()
+	rabbitConfig := amqp.Config{
+		Host:     amqpHost,
+		Port:     amqpPort,
+		LogLevel: 5,
+	}
+	serverConfig := amqp.ServerConfig{
+		ResponseX: "response",
+		RequestX:  "request",
+	}
+	sess := amqp.NewSession(rabbitConfig)
+	err := sess.Connect()
+	if err != nil {
+		return err
+	}
+	srv, err := sess.Server(serverConfig)
+	if err != nil {
+		return err
+	}
+	cfg := setdata_questionnaire_store.PostgresConfig{
+		Host:             postgresHost,
+		Port:             postgresPort,
+		User:             postgresUser,
+		Password:         postgresPassword,
+		Database:         postgresDatabaseName,
+		Params:           postgresParams,
+		ConnectionString: "",
+	}
+	questionPostgreStore, err := setdata_questionnaire_store.NewQuestionsPostgresStore(cfg)
+	if err != nil {
+		return err
+	}
+	questionService := setdata_questionnaire_store.NewQuestionsService(questionPostgreStore)
+	questionsAmqpEndpoints := setdata_questionnaire_store.NewQuestionsAmqpEndpoints(setdata_common.NewCommandHandler(questionService))
+	srv.Endpoint("questions.create", questionsAmqpEndpoints.MakeCreateQuestionAmqpEndpoint())
+	srv.Endpoint("questions.delete", questionsAmqpEndpoints.MakeDeleteQuestionAmqpEndpoint())
+	srv.Endpoint("questions.update", questionsAmqpEndpoints.MakeUpdateQuestionAmqpEndpoint())
+	srv.Endpoint("questions.list", questionsAmqpEndpoints.MakeListQuestionAmqpEndpoint())
+	srv.Endpoint("questions.get", questionsAmqpEndpoints.MakeGetQuestionAmqpEndpoint())
+	err = srv.Start()
+	if err != nil {
+		return err
+	}
+	return nil
 }
